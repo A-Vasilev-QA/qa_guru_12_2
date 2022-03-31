c90e19f019b0bd1b3c20dbc5655d361304668388
Aleksei_Vasilev <aleksei.vasilev@iii.com>
2022-03-31 20:55:27 +0300

    Added conflict

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello qa.guru");
         System.out.println("Let's make a CONFLICT in this line");
-        System.out.println("And add another one");
+        System.out.println("And add another one!");
     }
 
 }
