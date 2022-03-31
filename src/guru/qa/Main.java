e269fa05d6e928ed36084ecae521988f5d176617
Aleksei_Vasilev <aleksei.vasilev@iii.com>
2022-03-31 20:44:38 +0300

    added second line

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello qa.guru");
+        System.out.println("Let's make a conflict in this line");
     }
+
 }
